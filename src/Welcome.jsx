1e9da4a2c1d51edaae5c2d91a7a0e3e819b0bf78
Adam Borowski <adborowski@gmail.com>
2022-11-19 15:34:20 +0100

    Added basic handle for fig selection

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,10 +1,17 @@
 import styles from "./App.module.css";
 import Minifigs from "./components/minifigs/Minifigs";
+import { useState } from "react";
 const App = () => {
+  const [selectedSetId, setSelectedSetId] = useState();
+
+  const onFigSelection = (setId) => {
+    console.log(setId);
+  };
+
   return (
     <div className={styles.Welcome}>
       <h1 className={styles.headline}>Welcome to Lego Minifig randomizer!</h1>
-      <Minifigs />
+      <Minifigs onFigSelection={onFigSelection} />
     </div>
   );
 };
--- src/components/minifigs/Minifigs.jsx
+++ src/components/minifigs/Minifigs.jsx
@@ -2,63 +2,56 @@ import styles from "./Minifigs.module.css";
 import { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 const queryClient = new QueryClient();
-// https://rebrickable.com/api/v3/lego/minifigs/?in_theme_id=246&key=4a061f1fc0671c55d41c1d3991d185c9
-// data.results[Math.floor(Math.random() * data.count]
 
-const MinifigData = () => {
-  const { isLoading, error, data } = useQuery("repoData", () =>
-    fetch(
-      "https://rebrickable.com/api/v3/lego/minifigs/?in_theme_id=246&key=4a061f1fc0671c55d41c1d3991d185c9&page_size=400"
-    ).then((res) => res.json())
-  );
+const Minifigs = (props) => {
+  const MinifigData = () => {
+    const { isLoading, error, data } = useQuery("repoData", () =>
+      fetch(
+        "https://rebrickable.com/api/v3/lego/minifigs/?in_theme_id=246&key=4a061f1fc0671c55d41c1d3991d185c9&page_size=400"
+      ).then((res) => res.json())
+    );
 
-  const [randomMinifigs, setRandomMinifigs] = useState([]);
-  let howManyMinifigs = 3;
+    const [randomMinifigs, setRandomMinifigs] = useState([]);
+    let howManyMinifigs = 3; // how many random figs do we want (default 3)
 
-  useEffect(() => {
-    if (data) {
-      setRandomMinifigs([]);
-      let allMinifigs = data.results;
+    useEffect(() => {
+      if (data) {
+        setRandomMinifigs([]);
+        let allMinifigs = data.results;
 
-      // filter out the minifigs without an image
-      let filteredMinifigs = allMinifigs.filter(
-        (minifig) => minifig.set_img_url !== null
-      );
+        // filter out the minifigs without an image
+        let filteredMinifigs = allMinifigs.filter(
+          (minifig) => minifig.set_img_url !== null
+        );
 
-      // set the 3 random minifigs from the filtered set
-      for (let i = 0; i < howManyMinifigs; i++) {
-        setRandomMinifigs((randomMinifigs) => [
-          ...randomMinifigs,
-          filteredMinifigs[Math.floor(Math.random() * filteredMinifigs.length)],
-        ]);
+        // set the 3 random minifigs from the filtered set
+        for (let i = 0; i < howManyMinifigs; i++) {
+          setRandomMinifigs((randomMinifigs) => [
+            ...randomMinifigs,
+            filteredMinifigs[
+              Math.floor(Math.random() * filteredMinifigs.length)
+            ],
+          ]);
+        }
       }
-    }
-  }, [data]);
+    }, [data]);
 
-  useEffect(() => {
-    console.log(randomMinifigs);
-  }, [randomMinifigs]);
-
-  return randomMinifigs.map((randomMinifig) => (
-    <div
-      onClick={() => {
-        onMinifigSelect(randomMinifig.set_num);
-      }}
-      className={styles.randomMinifig}
-      key={randomMinifig.name}
-    >
+    return randomMinifigs.map((randomMinifig) => (
       <div
-        className={styles.image}
-        style={{ backgroundImage: `url(${randomMinifig.set_img_url})` }}
-      ></div>
-      <div className={styles.name}>{randomMinifig.name}</div>
-    </div>
-  ));
-};
-
-const Minifigs = () => {
-  console.log("Start");
-
+        onClick={() => {
+          props.onFigSelection(randomMinifig.set_num);
+        }}
+        className={styles.randomMinifig}
+        key={randomMinifig.name}
+      >
+        <div
+          className={styles.image}
+          style={{ backgroundImage: `url(${randomMinifig.set_img_url})` }}
+        ></div>
+        <div className={styles.name}>{randomMinifig.name}</div>
+      </div>
+    ));
+  };
   return (
     <QueryClientProvider client={queryClient}>
       <div className={styles.Minifigs}>
