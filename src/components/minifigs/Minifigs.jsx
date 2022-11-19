e74cde5e654206da3c0a6343ee4f974317bf3e86
Adam Borowski <adborowski@gmail.com>
2022-11-19 13:30:46 +0100

    Changed page_size to 400 and logged the resulting data

--- src/components/minifigs/Minifigs.jsx
+++ src/components/minifigs/Minifigs.jsx
@@ -4,28 +4,21 @@ import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 const queryClient = new QueryClient();
 // https://rebrickable.com/api/v3/lego/minifigs/?in_theme_id=246&key=4a061f1fc0671c55d41c1d3991d185c9
 
-function MinifigData() {
+const MinifigData = () => {
   const { isLoading, error, data } = useQuery("repoData", () =>
     fetch(
-      "https://rebrickable.com/api/v3/lego/minifigs/?in_theme_id=246&key=4a061f1fc0671c55d41c1d3991d185c9"
+      "https://rebrickable.com/api/v3/lego/minifigs/?in_theme_id=246&key=4a061f1fc0671c55d41c1d3991d185c9&page_size=400"
     ).then((res) => res.json())
   );
 
-  console.log(data);
-  if (isLoading) return "Loading...";
+  useEffect(() => {
+    if (data) {
+      console.log(data.results);
+    }
+  }, [data]);
 
-  if (error) return "An error has occurred: " + error.message;
-
-  return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>{data.description}</p>
-      <strong>👀 {data.subscribers_count}</strong>{" "}
-      <strong>✨ {data.stargazers_count}</strong>{" "}
-      <strong>🍴 {data.forks_count}</strong>
-    </div>
-  );
-}
+  return <div>a</div>;
+};
 
 const Minifigs = () => {
   console.log("Start");
