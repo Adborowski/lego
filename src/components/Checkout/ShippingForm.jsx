834a84a50d9b63cf1aa930a0f27b8685bf563a4d
Adam Borowski <adborowski@gmail.com>
2022-11-20 17:36:39 +0100

    Added logic for conditional marking of form fields as incorrect when there are errors (red border)

--- src/components/Checkout/ShippingForm.jsx
+++ src/components/Checkout/ShippingForm.jsx
@@ -46,22 +46,29 @@ const ShippingForm = () => {
         }}
       >
         {({ errors, touched }) => {
-          console.log("Errors: " + Object.keys(errors).length); // Log errors
-          console.log("Touches: " + Object.keys(touched).length); // Log errors
-
           return (
             <Form>
               <div className={styles.pair}>
-                <div className={styles.formItem}>
-                  <label for="firstName">First Name</label>
+                <div
+                  className={`${styles.formItem} ${
+                    errors.firstName && touched.firstName
+                      ? styles.incorrect
+                      : ""
+                  }`}
+                >
+                  <label>First Name</label>
                   <Field name="firstName" />
                   {errors.firstName && touched.firstName ? (
                     <div className={styles.errors}>{errors.firstName}</div>
                   ) : null}
                 </div>
 
-                <div className={styles.formItem}>
-                  <label for="lastName">Last Name</label>
+                <div
+                  className={`${styles.formItem} ${
+                    errors.lastName && touched.lastName ? styles.incorrect : ""
+                  }`}
+                >
+                  <label>Last Name</label>
                   <Field name="lastName" />
                   {errors.lastName && touched.lastName ? (
                     <div className={styles.errors}>{errors.lastName}</div>
@@ -70,16 +77,26 @@ const ShippingForm = () => {
               </div>
 
               <div className={styles.pair}>
-                <div className={styles.formItem}>
-                  <label for="email">Email</label>
+                <div
+                  className={`${styles.formItem} ${
+                    errors.email && touched.email ? styles.incorrect : ""
+                  }`}
+                >
+                  <label>Email</label>
                   <Field name="email" type="email" />
                   {errors.email && touched.email ? (
                     <div className={styles.errors}>{errors.email}</div>
                   ) : null}
                 </div>
 
-                <div className={styles.formItem}>
-                  <label for="phoneNumber">Phone Number</label>
+                <div
+                  className={`${styles.formItem} ${
+                    errors.phoneNumber && touched.phoneNumber
+                      ? styles.incorrect
+                      : ""
+                  }`}
+                >
+                  <label>Phone Number</label>
                   <Field name="phoneNumber" />
                   {errors.phoneNumber && touched.phoneNumber ? (
                     <div className={styles.errors}>{errors.phoneNumber}</div>
@@ -87,42 +104,64 @@ const ShippingForm = () => {
                 </div>
               </div>
 
-              <div className={styles.formItem}>
-                <label for="dateOfBirth">Date of Birth</label>
+              <div
+                className={`${styles.formItem} ${
+                  errors.dateOfBirth && touched.dateOfBirth
+                    ? styles.incorrect
+                    : ""
+                }`}
+              >
+                <label>Date of Birth</label>
                 <Field name="dateOfBirth" type="date" />
                 {errors.dateOfBirth && touched.dateOfBirth ? (
                   <div className={styles.errors}>{errors.dateOfBirth}</div>
                 ) : null}
               </div>
 
-              <div className={styles.formItem}>
-                <label for="address">Address</label>
+              <div
+                className={`${styles.formItem} ${
+                  errors.address && touched.address ? styles.incorrect : ""
+                }`}
+              >
+                <label>Address</label>
                 <Field name="address" />
                 {errors.address && touched.address ? (
                   <div className={styles.errors}>{errors.address}</div>
                 ) : null}
               </div>
 
-              <div className={styles.trio}>
-                <div className={styles.formItem}>
-                  <label for="city">City</label>
-                  <Field name="city" />
-                  {errors.city && touched.city ? (
-                    <div className={styles.errors}>{errors.city}</div>
-                  ) : null}
-                </div>
+              <div
+                className={`${styles.formItem} ${
+                  errors.city && touched.city ? styles.incorrect : ""
+                }`}
+              >
+                <label>City</label>
+                <Field name="city" />
+                {errors.city && touched.city ? (
+                  <div className={styles.errors}>{errors.city}</div>
+                ) : null}
+              </div>
 
-                <div className={styles.formItem}>
+              <div className={styles.duo}>
+                <div
+                  className={`${styles.formItem} ${
+                    errors.state && touched.state ? styles.incorrect : ""
+                  }`}
+                >
                   {" "}
-                  <label for="state">State Code (e.g. CA)</label>
+                  <label>State Code (e.g. CA)</label>
                   <Field name="state" />
                   {errors.state && touched.state ? (
                     <div className={styles.errors}>{errors.state}</div>
                   ) : null}
                 </div>
-                <div className={styles.formItem}>
+                <div
+                  className={`${styles.formItem} ${
+                    errors.zipCode && touched.zipCode ? styles.incorrect : ""
+                  }`}
+                >
                   {" "}
-                  <label for="zipCode">ZipCode</label>
+                  <label>ZipCode</label>
                   <Field name="zipCode" />
                   {errors.zipCode && touched.zipCode ? (
                     <div className={styles.errors}>{errors.zipCode}</div>
