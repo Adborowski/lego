2f5a23370cc1e96ee67dedb78a313817653aecdf
Adam Borowski <adborowski@gmail.com>
2022-11-20 14:43:15 +0100

    Added Formik and Yup and made a boilerplate shipping form

--- src/components/Checkout/ShippingForm.jsx
+++ src/components/Checkout/ShippingForm.jsx
@@ -1,5 +1,55 @@
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
 const ShippingForm = () => {
-  return <h2>Shipping Form</h2>;
+  const ShippingSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    lastName: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+  });
+
+  return (
+    <div>
+      <h2>Shipping Form</h2>
+
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        validationSchema={ShippingSchema}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Field name="firstName" />
+            {errors.firstName && touched.firstName ? (
+              <div>{errors.firstName}</div>
+            ) : null}
+
+            <Field name="lastName" />
+            {errors.lastName && touched.lastName ? (
+              <div>{errors.lastName}</div>
+            ) : null}
+
+            <Field name="email" type="email" />
+            {errors.email && touched.email ? <div>{errors.email}</div> : null}
+
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
 
 export default ShippingForm;
