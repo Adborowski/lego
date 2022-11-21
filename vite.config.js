a56d2ece8647eb7941cbe5dac9028ceadcf3d74f
Adam Borowski <adborowski@gmail.com>
2022-11-21 13:56:29 +0100

    Changed favicon to Lego logo

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import htmlPlugin from "vite-plugin-html-config";
 
 const htmlPluginOpt = {
   title: "Lego Mystery Box",
+  favicon: "/react.svg",
 };
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), htmlPlugin(htmlPluginOpt)],
-  base: "./",
 });
